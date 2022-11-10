17a207b4e5aaa07f8e8e2e37839a4ccde8778c93
niladri26 <105287219+niladri26@users.noreply.github.com>
2022-11-10 13:31:14 +0530

    Add files via upload

---
+++ StudentManagement/src/main/java/com/greatLearning/studentManagement/StudentManagementApplication.java
@@ -0,0 +1,15 @@
+package com.greatLearning.studentManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication.class, args);
+		
+		System.out.println("---------------------------END---------------------------");
+	}
+
+}
